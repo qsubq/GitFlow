e39ae8cac89460a84cec8819ab10f2c92eb94318
Maksim <maksim-kargin@bk.ru>
2023-07-20 13:25:04 +0300

    authFragment is ready

--- feature/signIn/src/main/java/com/example/signin/auth/AuthFragment.kt
+++ feature/signIn/src/main/java/com/example/signin/auth/AuthFragment.kt
@@ -1,19 +1,51 @@
 package com.example.signin.auth
 
 import android.os.Bundle
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.ClickableSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.core.utils.navigate
 import com.example.signin.R
-import com.example.signin.databinding.FragmentAuthBinding
-import com.jakewharton.rxbinding4.widget.textChanges
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.functions.BiFunction
@@ -21,74 +53,281 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 class AuthFragment : Fragment() {
-    private lateinit var binding: FragmentAuthBinding
+    private lateinit var composeAuthScreen: ComposeView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = FragmentAuthBinding.inflate(layoutInflater, container, false)
-        return binding.root
+        return ComposeView(requireContext()).also {
+            composeAuthScreen = it
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.btnSignIn.setOnClickListener {
-            this.navigate(R.id.action_authFragment_to_containerFragment)
+        composeAuthScreen.setContent {
+            Column {
+                Toolbar()
+                MainScreen()
+            }
         }
-        spannableText()
-        eTObservable()
     }
 
-    private fun eTObservable() {
-        val loginObservable: Observable<String> = binding.ETEmail.textChanges()
-            .map { it.toString() }
-            .debounce(500, TimeUnit.MILLISECONDS)
+    //    private fun eTObservable() {
+//        val loginObservable: Observable<String> = binding.ETEmail.textChanges()
+//            .map { it.toString() }
+//            .debounce(500, TimeUnit.MILLISECONDS)
+//
+//        val observable = Observable.combineLatest(
+//            loginObservable,
+//            passwordObservable,
+//            BiFunction<String, String, Boolean> { login, password ->
+//                login.length >= 6 && password.length >= 6
+//            },
+//        )
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe { canLogin ->
+//                binding.btnSignIn.isEnabled = canLogin
+//            }
+//    }
 
-        val passwordObservable: Observable<String> = binding.ETPassword.textChanges()
-            .map { it.toString() }
-            .debounce(500, TimeUnit.MILLISECONDS)
+    private fun showToast(text: String) {
+        Toast.makeText(
+            requireContext(),
+            text,
+            Toast.LENGTH_SHORT,
+        ).show()
+    }
 
+    @Composable
+    fun Toolbar() {
+        Box(
+            modifier = Modifier
+                .background(Color(0xff66a636))
+                .height(56.dp)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.arrow_back),
+                contentDescription = "Image back",
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .padding(start = 24.dp),
+            )
+            Text(
+                text = "Авторизация",
+                fontSize = 21.sp,
+                color = Color.White,
+                fontFamily = FontFamily(Font(resId = R.font.officinasansextraboldscc)),
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun MainScreen() {
+        val inputEmail = remember { mutableStateOf("") }
+        val inputPassword = remember { mutableStateOf("") }
+        val btnState = remember { mutableStateOf(false) }
+
+        val emailObservable: Observable<String> = Observable.just(inputEmail.value)
+        val passwordObservable: Observable<String> = Observable.just(inputPassword.value)
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "Для участия в мероприятиях войдите\nв приложение через социальые сети",
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default,
+                color = Color(0xb3000000),
+                textAlign = TextAlign.Center,
+            )
+            Row(modifier = Modifier.padding(start = 80.dp, end = 80.dp, top = 20.dp)) {
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.vk_icon),
+                    contentDescription = "vkIcon",
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.facebook_icon),
+                    contentDescription = "facebookIcon",
+                    modifier = Modifier.padding(start = 40.dp),
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.classmate_icon),
+                    contentDescription = "odIcon",
+                    modifier = Modifier.padding(start = 40.dp),
+                )
+            }
+            Text(
+                text = "Или авторизуйтесь через приложение",
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default,
+                color = Color(0xb3000000),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 40.dp),
+            )
+
+            Text(
+                text = "E-mail",
+                fontSize = 12.sp,
+                fontFamily = FontFamily.Default,
+                color = Color(0x61000000),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top = 20.dp, start = 20.dp)
+                    .align(Alignment.Start),
+            )
+            BasicTextField(
+                value = inputEmail.value,
+                onValueChange = { it ->
+                    inputEmail.value = it
+                    emailObservable
+                        .map { it }
+                        .debounce(500, TimeUnit.MILLISECONDS)
+                },
+                modifier = Modifier
+                    .padding(top = 8.dp, start = 20.dp, end = 20.dp)
+                    .fillMaxWidth()
+                    .height(48.dp),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                decorationBox = { innerTextField ->
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Row {
+                            if (inputEmail.value.isEmpty()) {
+                                Text(
+                                    text = "Введите e-mail",
+                                    color = Color(0x61000000),
+                                    fontSize = 16.sp,
+                                    fontFamily = FontFamily.Default,
+                                )
+                            }
+                            //
+                            innerTextField()
+                        }
+                        Divider(color = Color(0x1e000000), modifier = Modifier.padding(top = 8.dp))
+                    }
+                },
+            )
+            Text(
+                text = "Пароль",
+                fontSize = 12.sp,
+                fontFamily = FontFamily.Default,
+                color = Color(0x61000000),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top = 20.dp, start = 20.dp)
+                    .align(Alignment.Start),
+            )
+            BasicTextField(
+                value = inputPassword.value,
+                onValueChange = {
+                    inputPassword.value = it
+                    passwordObservable
+                        .map { it }
+                        .debounce(500, TimeUnit.MILLISECONDS)
+                },
+                modifier = Modifier
+                    .padding(top = 8.dp, start = 20.dp, end = 20.dp)
+                    .fillMaxWidth()
+                    .height(48.dp),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                decorationBox = { innerTextField ->
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Row {
+                            if (inputPassword.value.isEmpty()) {
+                                Text(
+                                    text = "Введите пароль",
+                                    color = Color(0x61000000),
+                                    fontSize = 16.sp,
+                                    fontFamily = FontFamily.Default,
+                                )
+                            }
+                            //
+                            innerTextField()
+                        }
+                        Divider(color = Color(0x1e000000), modifier = Modifier.padding(top = 8.dp))
+                    }
+                },
+            )
+            Button(
+                onClick = {
+                    navigate(R.id.action_authFragment_to_containerFragment)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 20.dp, end = 20.dp, top = 26.dp)
+                    .height(44.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xff66a636)),
+                shape = RoundedCornerShape(2.dp),
+                enabled = btnState.value
+            ) {
+                Text(text = "ВОЙТИ", fontSize = 16.sp)
+            }
+            val annotatedForgetPassword = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color(0xff66a636),
+                        fontSize = 14.sp,
+                        textDecoration = TextDecoration.Underline,
+                    ),
+                ) {
+                    append("Забыли пароль?")
+                }
+            }
+            val annotatedRegistration = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color(0xff66a636),
+                        fontSize = 14.sp,
+                        textDecoration = TextDecoration.Underline,
+                    ),
+                ) {
+                    append("Регистрация")
+                }
+            }
+
+            Box(
+                modifier = Modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp).fillMaxWidth(),
+            ) {
+                ClickableText(
+                    modifier = Modifier.align(Alignment.CenterStart),
+                    text = annotatedForgetPassword,
+                    onClick = {
+                        showToast("Забыли пароль")
+                    },
+                )
+
+                ClickableText(
+                    modifier = Modifier.align(Alignment.CenterEnd),
+                    text = annotatedRegistration,
+                    onClick = {
+                        showToast("Забыли пароль")
+                    },
+                )
+            }
+        }
         val observable = Observable.combineLatest(
-            loginObservable,
+            emailObservable,
             passwordObservable,
             BiFunction<String, String, Boolean> { login, password ->
                 login.length >= 6 && password.length >= 6
             },
-        )
-            .subscribeOn(Schedulers.io())
+        ).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { canLogin ->
-                binding.btnSignIn.isEnabled = canLogin
+                btnState.value = canLogin
             }
-    }
-
-    private fun spannableText() {
-        val forgerPasswordSpanText =
-            SpannableStringBuilder(requireContext().getString(R.string.forget_password))
-        val clickableForgerPasswordSpan = object : ClickableSpan() {
-            override fun onClick(widget: View) {}
+        SideEffect {
+            Log.e("Maksim", "The side effect")
         }
-        forgerPasswordSpanText.setSpan(
-            clickableForgerPasswordSpan,
-            0,
-            forgerPasswordSpanText.lastIndex,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
-        )
-        binding.tvForgerPassword.setText(forgerPasswordSpanText, TextView.BufferType.SPANNABLE)
-
-        val registrationSpanText =
-            SpannableStringBuilder(requireContext().getString(R.string.forget_password))
-        val clickableRegistrationSpan = object : ClickableSpan() {
-            override fun onClick(widget: View) {}
-        }
-        registrationSpanText.setSpan(
-            clickableRegistrationSpan,
-            0,
-            forgerPasswordSpanText.lastIndex,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
-        )
-        binding.tvRegistration.setText(registrationSpanText, TextView.BufferType.SPANNABLE)
     }
 }
